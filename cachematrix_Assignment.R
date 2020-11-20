284d88b893c4f97a4cb29ada6bf3ae2c9d0f4fda
pratikb4668 <74422014+pratikb4668@users.noreply.github.com>
2020-11-20 14:24:10 +0530

    Add files via upload

---
+++ cachematrix_Assignment.R
@@ -0,0 +1,53 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+
+makeCacheMatrix <- function(x = matrix()) {
+  
+}
+
+
+## Write a short comment describing this function
+
+cacheSolve <- function(x, ...) {
+  ## Return a matrix that is the inverse of 'x'
+}
+
+# Solve
+
+makeCacheMatrix <- function(x = matrix()) {
+  s <- NULL
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) s <<- solve
+  getsolve <- function() s
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
+}
+
+
+cacheSolve <- function(x, ...) {
+  s <- x$getsolve()
+  if(!is.null(s)) {
+    message("getting inversed matrix")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data, ...)
+  x$setsolve(s)
+  s
+}
+
+# Test
+
+my.matrix <- matrix(rnorm(25),5,5)
+
+my.matrix.1 <- makeCacheMatrix(my.matrix)
+
+cacheSolve(my.matrix.1)
+    
